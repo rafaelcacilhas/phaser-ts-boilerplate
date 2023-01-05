b2c42d436e04301e209dc095f75ad801a6c24eed
Rafael Cacilhas <rafaelcacilhas@gmail.com>
2023-01-05 17:07:56 -0300

    typescript boilerplate for Phaser

---
+++ src/game.ts
@@ -0,0 +1,27 @@
+import Phaser from "phaser";
+import MainScene from "./mainScene";
+
+const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.WEBGL,
+    width: 1920,
+    height: 1080,
+    scale: {
+        mode: Phaser.Scale.ENVELOP,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    fps: {
+        target: 12,
+        forceSetTimeOut: true,
+    },
+    backgroundColor: "#0000000",
+	physics:{
+		default: 'arcade',
+		arcade:{
+			gravity: {y: 200},
+			debug:false
+		},
+	},
+    scene: [MainScene],
+};
+
+export default new Phaser.Game(config)
